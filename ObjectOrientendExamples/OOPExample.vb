7e46c3908e590321730661a871987943a8397f84
Zac <zac.minidude140@Gmail.com>
2023-09-26 08:56:05 -0600

    Added Student ID increment.  Added Irishman Class

--- ObjectOrientendExamples/OOPExample.vb
+++ ObjectOrientendExamples/OOPExample.vb
@@ -12,15 +12,20 @@ Option Strict On
 'Generic object class system
 'developed to organize..
 Module OOPExample
-
+    Dim idCount As Integer = 0
     Sub Main()
         Dim personOne As New Person("Billy", 45)
-        Dim Student As New Student("Sally", 21)
+        Dim studentOne As New Student("Sally", 21)
+        Dim StudentTwo As New Student("Bob", 25)
+        Dim SoapGuy As New IrishMan("Aiofe", 30)
 
         Console.WriteLine(personOne.Name & vbLf & personOne.Age & vbLf)
         Console.WriteLine()
-        Console.WriteLine(Student.Name & vbLf & Student.Age & vbLf & Student.idNumber)
+        Console.WriteLine(studentOne.Name & vbLf & studentOne.Age & vbLf & studentOne.idNumber & vbLf)
+        Console.WriteLine(StudentTwo.Name & vbLf & StudentTwo.Age & vbLf & StudentTwo.idNumber & vbLf)
+        Console.WriteLine(SoapGuy.Name & vbLf & SoapGuy.Age & vbLf & SoapGuy.irishComment & vbLf)
         Console.Read()
+
     End Sub
 
     Class Person
@@ -34,14 +39,24 @@ Module OOPExample
 
     Class Student
         Inherits Person
-        Public idNumber As String
+        Public idNumber As Integer
         Public Sub New(Name As String, Age As Integer)
             MyBase.New(Name, Age)
-            Me.idNumber = "1234"
+            Me.idNumber = idCount
+            idCount += 1
 
         End Sub
         Public StudentID As Integer
 
+    End Class
+
+    Class IrishMan
+        Inherits Person
+        Public irishComment As String
+        Public Sub New(Name As String, Age As Integer)
+            MyBase.New(Name, Age)
+            Me.irishComment = "Good to be at the top the mountain"
+        End Sub
     End Class
 
 End Module
